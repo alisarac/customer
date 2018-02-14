2fef741572011d59f72ee6db40268e7503732c1e
Ali Saraç <alisarac@gmail.com>
2018-02-14 18:25:05 +0300

    update customer

--- src/test/kotlin/com/microservices/chapter11/CustomerControllerTest.kt
+++ src/test/kotlin/com/microservices/chapter11/CustomerControllerTest.kt
@@ -28,7 +28,7 @@ class CustomerControllerTest {
                 .andExpect(jsonPath("\$[1].id").value(2))
                 .andExpect(jsonPath("\$[1].name").value("Spring"))
                 .andExpect(jsonPath("\$[2].id").value(3))
-                .andExpect(jsonPath("\$[2].name").value("Microservice"))
+                .andExpect(jsonPath("\$[2].name").value("Microservices"))
                 .andExpect(jsonPath("\$[3].id").value(4))
                 .andExpect(jsonPath("\$[3].name").value("OpenShift"))
                 .andDo(print())
